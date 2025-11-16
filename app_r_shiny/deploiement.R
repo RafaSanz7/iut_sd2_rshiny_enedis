1cf41016edfb0e18d8c8efbb7bed97b0adafc52c
srafanz <rafael.sanz.pro@gmail.com>
2025-11-16 20:37:29 +0100

    update

---
+++ app_r_shiny/deploiement.R
@@ -0,0 +1,7 @@
+install.packages("rsconnect")
+
+rsconnect::setAccountInfo(name='quentin74',
+                          token='76EDCBDB381E215C660FD1E551B9D839',
+                          secret='U92P78U3qRI5RM74muhTjg1RXjUO2rmyLwf6VVny')
+
+rsconnect::deployApp("C:/Users/utilisateur/OneDrive - univ-lyon2.fr/Espace Travail/Projet-r/App_Shiny")
